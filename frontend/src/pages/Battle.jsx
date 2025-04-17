b001de3ef678a951a9f2719de765c9b64b96aae9
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-17 13:59:08 -0300

    feat: add routing with Home and Battle pages in App

---
+++ frontend/src/pages/Battle.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { socket } from "../socket";
+
+import { joinArena } from "../services/arena";
+import { randomMonster } from "../services/monster";
+import { CardOne } from "../components/CardOne";
+import { CardTwo } from "../components/CardTwo";
+
+export const Battle = () => {
+  const [isTwoPlayers, setIsTwoPlayers] = useState(false);
+  const [monsterOne, setMonsterOne] = useState({
+    name: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+  });
+  const [monsterTwo, setMonsterTwo] = useState({
+    name: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+  });
+
+  useEffect(() => {
+    const result = async () => {
+      const response = await joinArena(5, 3);
+      const monster = await randomMonster(response["monster_id"]);
+      setMonsterOne(monster);
+      const socketId = socket.id.toString();
+      socket.emit("userJoinRoom", { [socketId]: { monster } });
+
+      const response_ = await joinArena(5, 3);
+      const monster_ = await randomMonster(response_["monster_id"]);
+      setMonsterTwo(monster_);
+    };
+
+    socket.on("roomInfo", (data) => {
+      if (Object.keys(data).length === 2) {
+        setIsTwoPlayers(true);
+      }
+    });
+
+    socket.on("userDisconnected", (data) => {
+      console.log("userDisconnected");
+    });
+
+    result();
+  }, []);
+  return (
+    <>
+      {isTwoPlayers ? (
+        <div className="flex">
+          <CardOne
+            name={monsterOne.name}
+            hp={monsterOne.hp}
+            attack={monsterOne.attack}
+            defense={monsterOne.defense}
+            speed={monsterOne.speed}
+          />
+          <CardTwo
+            name={monsterTwo.name}
+            hp={monsterTwo.hp}
+            attack={monsterTwo.attack}
+            defense={monsterTwo.defense}
+            speed={monsterTwo.speed}
+          />
+        </div>
+      ) : (
+        <div className="font-bold text-2xl">Waiting another player...</div>
+      )}
+    </>
+  );
+};
