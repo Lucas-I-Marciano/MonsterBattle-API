0687a33302b73dfd727260a4531a049264365c67
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-17 09:17:35 -0300

    feat: render cards only when two players are connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { joinArena } from "./services/arena";
 import { randomMonster } from "./services/monster";
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [isTwoPlayers, setIsTwoPlayers] = useState(false);
   const [monsterOne, setMonsterOne] = useState({
     name: "",
     hp: "",
@@ -25,48 +25,52 @@ function App() {
   });
 
   useEffect(() => {
-    const onConnect = () => setIsConnected(true);
-    const onDisconnect = () => setIsConnected(false);
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
     const result = async () => {
       const response = await joinArena(5, 3);
       const monster = await randomMonster(response["monster_id"]);
       setMonsterOne(monster);
+      const socketId = socket.id.toString();
+      socket.emit("userJoinRoom", { [socketId]: { monster } });
 
       const response_ = await joinArena(5, 3);
       const monster_ = await randomMonster(response_["monster_id"]);
       setMonsterTwo(monster_);
     };
+
+    socket.on("roomInfo", (data) => {
+      if (Object.keys(data).length === 2) {
+        setIsTwoPlayers(true);
+      }
+    });
+
+    socket.on("userDisconnected", (data) => {
+      console.log("userDisconnected");
+    });
+
     result();
   }, []);
   return (
     <>
-      <div className="App flex flex-col"></div>
-      <button
-        onClick={() => {
-          socket.emit("testEvent");
-        }}
-      >
-        testEvent
-      </button>
-      <div className="flex">
-        <CardOne
-          name={monsterOne.name}
-          hp={monsterOne.hp}
-          attack={monsterOne.attack}
-          defense={monsterOne.defense}
-          speed={monsterOne.speed}
-        />
-        <CardTwo
-          name={monsterTwo.name}
-          hp={monsterTwo.hp}
-          attack={monsterTwo.attack}
-          defense={monsterTwo.defense}
-          speed={monsterTwo.speed}
-        />
-      </div>
+      {isTwoPlayers ? (
+        <div className="flex">
+          <CardOne
+            name={monsterOne.name}
+            hp={monsterOne.hp}
+            attack={monsterOne.attack}
+            defense={monsterOne.defense}
+            speed={monsterOne.speed}
+          />
+          <CardTwo
+            name={monsterTwo.name}
+            hp={monsterTwo.hp}
+            attack={monsterTwo.attack}
+            defense={monsterTwo.defense}
+            speed={monsterTwo.speed}
+          />
+        </div>
+      ) : (
+        <div className="font-bold text-2xl">Waiting another player...</div>
+      )}
     </>
   );
 }
