6c19f2cf75611a99b8bf5f7b867d0e3a73e9e934
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-16 22:01:42 -0300

    feat: implement route to fetch monster by ID

--- backend/src/routers/monsters.js
+++ backend/src/routers/monsters.js
@@ -1,49 +1,82 @@
 import { Router } from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 router
-    .post("/", async (req, res) => {
-        const { name, hp, attack, defense, speed, special } = req.body
-        const createObject = { name, hp, attack, defense, speed, special }
-        const haveNullValue = Object.entries(createObject).filter((info)=>{return Boolean(!info[1])})
-        
-        if (haveNullValue.length > 0) {
-            return res.status(400).json({"message":"fail", "detail":"All fields are required - name, hp, attack, defense, speed, special"})
-        }
+  .post("/", async (req, res) => {
+    const { name, hp, attack, defense, speed, special } = req.body;
+    const createObject = { name, hp, attack, defense, speed, special };
+    const haveNullValue = Object.entries(createObject).filter((info) => {
+      return Boolean(!info[1]);
+    });
 
-        try {
-            const monster = await prisma.monster.findUnique({
-                where: {
-                    name
-                }
-            })
-            if (monster) {
-                return res.status(400).json({"message":"fail", "detail":"Monster already exists"})
-            }
+    if (haveNullValue.length > 0) {
+      return res
+        .status(400)
+        .json({
+          message: "fail",
+          detail:
+            "All fields are required - name, hp, attack, defense, speed, special",
+        });
+    }
 
-            const result = await prisma.monster.create({
-                data: createObject
-            })
-            res.status(201).json({
-                "message": "created",
-                "data": result
-            })
-        } catch (error) {
-            res.status(500).json({"message":"fail", "detail":"Internal Server Error"})
-        }
-    })
-    .get('/', async (req, res) => {
+    try {
+      const monster = await prisma.monster.findUnique({
+        where: {
+          name,
+        },
+      });
+      if (monster) {
+        return res
+          .status(400)
+          .json({ message: "fail", detail: "Monster already exists" });
+      }
 
-        try {
-            const monsters = await prisma.monster.findMany()
-            res.status(200).json(monsters)
-        } catch (error) {
-            res.status(500).json({"message":"fail", "detail":"Internal Server Error"})
-        }
-    })
+      const result = await prisma.monster.create({
+        data: createObject,
+      });
+      res.status(201).json({
+        message: "created",
+        data: result,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "fail", detail: "Internal Server Error" });
+    }
+  })
+  .get("/", async (req, res) => {
+    try {
+      const monsters = await prisma.monster.findMany();
+      res.status(200).json(monsters);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "fail", detail: "Internal Server Error" });
+    }
+  })
+  .get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const monster = await prisma.monster.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      if (!monster) {
+        return res
+          .status(404)
+          .json({ message: "fail", detail: "Monster not founded" });
+      }
 
-export default router
+      return res.status(200).json(monster);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "fail", detail: "Internal Server Error" });
+    }
+  });
 
+export default router;
