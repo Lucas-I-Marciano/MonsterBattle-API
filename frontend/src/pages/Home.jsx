7326ff9da1caf636f7e43a731a09202f50683260
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-18 00:55:09 -0300

    feat: add player form with react-hook-form, yup validation and dynamic button state using useReducer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,79 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { createPlayer } from "../services/player";
+import { socket } from "../socket";
+import { useReducer } from "react";
+
+const schema = yup
+  .object({
+    name: yup.string().required("Please choose a name"),
+  })
+  .required();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        isLoading: true,
+        text: "Loading...",
+        disabled: true,
+      };
+    default:
+      return state;
+  }
+};
+
 export const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = ({ name }) => {
+    dispatch({ type: "loading" });
+    createPlayer({ name, socket: socket.id.toString() });
+  };
+
+  const [buttonState, dispatch] = useReducer(reducer, {
+    isLoading: false,
+    text: "Create",
+    disabled: false,
+  });
   return (
-    <h1 className="w-full text-center text-2xl font-bold bg-blue-100">Home</h1>
+    <>
+      <h1 className="w-full text-center text-2xl font-bold p-10">
+        Welcome to Monster Battle 🧌
+      </h1>
+      <form
+        className="w-full flex flex-col items-center text-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="">
+          Name
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            {...register("name")}
+          />
+          <p className="text-red-500 text-sm font-bold">
+            {errors.name?.message}
+          </p>
+        </label>
+
+        <button
+          disabled={buttonState.disabled}
+          className={`w-30 border rounded-md mt-10 ${
+            buttonState.disabled ? "bg-gray-300 text-white" : "bg-gray-100"
+          }`}
+          type="submit"
+        >
+          {buttonState.text}
+        </button>
+      </form>
+    </>
   );
 };
