8f200664d1564eb9dad001e51aa5b14ef89321ad
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-18 00:53:44 -0300

    feat: add player service with create and delete functions

---
+++ frontend/src/services/player.js
@@ -0,0 +1,11 @@
+import client from "../providers/client";
+
+export const createPlayer = async (data) => {
+  const response = await client.post(`/players`, data);
+  return response.data;
+};
+
+export const deletePlayer = async (id) => {
+  const response = await client.post(`/players/${id}`);
+  return response.data;
+};
