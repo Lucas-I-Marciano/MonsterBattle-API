311901f5f8cd9236b8f8293a794d290c80f79e02
Lucas Ioran Marciano <lucas.marciano99@outlook.com>
2025-04-17 23:21:20 -0300

    feat: emit userJoinRoom with socket.id and monster on app load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import { Routes, Route } from "react-router";
 import { Battle } from "./pages/Battle";
-
-const Home = () => {
-  return (
-    <h1 className="w-full text-center text-2xl font-bold bg-blue-100">Home</h1>
-  );
-};
+import { Home } from "./pages/Home";
+import { socket } from "./socket";
+import { useEffect } from "react";
+import { joinArena } from "./services/arena";
+import { randomMonster } from "./services/monster";
 
 function App() {
+  useEffect(() => {
+    const result = async () => {
+      const response = await joinArena(5, 3);
+      const monster = await randomMonster(response["monster_id"]);
+      const socketId = socket.id.toString();
+
+      socket.emit("userJoinRoom", { [socketId]: { monster } });
+    };
+
+    socket.on("userDisconnected", (data) => {
+      console.log("userDisconnected");
+    });
+
+    result();
+  }, []);
+
   return (
     <>
       <Routes>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+  return (
+    <h1 className="w-full text-center text-2xl font-bold bg-blue-100">Home</h1>
+  );
+};
